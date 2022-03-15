5b43e776d348ccba8113998b0aa6f212f0bbb595
Igor <idmitrievnz@gmail.com>
2022-03-15 16:58:36 +1300

    Lesson 8: Added ViewBindingDelegate

--- app/src/main/java/com/example/android/githubuser/ui/repos/ReposFragment.kt
+++ app/src/main/java/com/example/android/githubuser/ui/repos/ReposFragment.kt
@@ -1,48 +1,33 @@
 package com.example.android.githubuser.ui.repos
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.os.bundleOf
 import com.example.android.githubuser.App
+import com.example.android.githubuser.R
 import com.example.android.githubuser.databinding.FragmentReposBinding
 import com.example.android.githubuser.domain.model.GitHubRepoDetailModel
 import com.example.android.githubuser.domain.model.GithubUserModel
 import com.example.android.githubuser.ui.base.BackButtonListener
+import com.example.android.githubuser.ui.base.viewBinding
 import com.example.android.githubuser.ui.repos.adapter.ReposAdapter
 import moxy.MvpAppCompatFragment
 import moxy.ktx.moxyPresenter
 
-class ReposFragment : MvpAppCompatFragment(), ReposView, BackButtonListener {
+class ReposFragment : MvpAppCompatFragment(R.layout.fragment_repos), ReposView, BackButtonListener {
 
     private val userModel by lazy {
         requireArguments().getParcelable<GithubUserModel>(KEY_USER_MODEL)!!
     }
 
     private val presenter by moxyPresenter {
-       // ReposPresenter(userModel)
+        // ReposPresenter(userModel)
         App.instance.appComponent.provideReposPresenterFactory().presenter(userModel)
     }
 
     private val adapter by lazy { ReposAdapter() { presenter.onItemClicked() } }
-    private var _binding: FragmentReposBinding? = null
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentReposBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        _binding = null
-    }
+    private val binding: FragmentReposBinding by viewBinding()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
